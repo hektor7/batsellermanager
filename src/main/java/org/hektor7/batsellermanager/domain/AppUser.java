7b1d69a1862f9ff4679f3c13de57429bb10671fe
Héctor Vizcaíno Cortegano <hektor7@gmail.com>
2015-05-03 14:41:32 +0200

    New TODOs

--- src/main/java/org/hektor7/batsellermanager/domain/AppUser.java
+++ src/main/java/org/hektor7/batsellermanager/domain/AppUser.java
@@ -18,6 +18,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.security.authentication.encoding.MessageDigestPasswordEncoder;
 
+//TODO: User have to have at least one main ContactInfo
 /**
  * Entity implementation class for Entity: AppUser
  *
