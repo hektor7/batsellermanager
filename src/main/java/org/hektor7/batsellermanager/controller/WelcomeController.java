1912ab5efb6af9ef24d1b7f5724dc50b4eaf0372
Héctor Vizcaíno Cortegano <hektor7@gmail.com>
2015-05-07 20:05:33 +0200

    log4j error has been solved ;)

--- src/main/java/org/hektor7/batsellermanager/controller/WelcomeController.java
+++ src/main/java/org/hektor7/batsellermanager/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package org.hektor7.batsellermanager.controller;
 
+import org.jboss.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 
+	Logger logger = Logger.getLogger(this.getClass());
+	
 	@RequestMapping("/")
 	public String welcome(Model model) {
+		
+		this.logger.debug("Entering into welcome page :) ");
 		
 		return "welcome";
 	}
